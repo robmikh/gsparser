448477d1a29143a3874f131e9fdb4db967979894
robmikh <rob.mikh@outlook.com>
2024-10-25 02:07:58 -0700

    refactor bspaudit

--- examples/bspaudit.rs
+++ examples/bspaudit.rs
@@ -3,25 +3,37 @@ extern crate gsparser;
 
 use glob::glob;
 use gsparser::bsp::BspReader;
-use std::env;
+use std::{env, path::PathBuf};
 
 fn main() {
     let args = env::args().skip(1).collect::<Vec<_>>();
     let path = args.get(0).unwrap();
 
+    let maps = collect_maps(&path);
+
+    print_max_num_textures(&maps);
+}
+
+fn collect_maps(path: &str) -> Vec<PathBuf> {
+    let mut paths = Vec::new();
     let search = format!("{}/**/*.bsp", path);
-    println!("search: {}", search);
     let bsps = glob(&search).unwrap();
-    let mut max = u32::MIN;
     for bsp in bsps {
         let bsp = bsp.unwrap();
+        paths.push(bsp);
+    }
+    paths
+}
 
-        let data = std::fs::read(&bsp).unwrap();
+fn print_max_num_textures(paths: &[PathBuf]) {
+    let mut max = u32::MIN;
+    for bsp_path in paths {
+        let data = std::fs::read(bsp_path).unwrap();
         let reader = BspReader::read(data);
         let textures = reader.read_textures_header();
         let num_textures = textures.num_textures;
 
-        println!("bsp: {}  -  {}", bsp.display(), num_textures);
+        println!("bsp: {}  -  {}", bsp_path.display(), num_textures);
         max = max.max(num_textures);
     }
     println!("Max num textures: {}", max);
