8a7fad61d66eacf4eb9f355085350bb00ca9acbb
robmikh <rob.mikh@outlook.com>
2025-04-27 15:43:37 -0700

    tools

---
+++ examples/bspentitydump.rs
@@ -0,0 +1,59 @@
+extern crate glob;
+extern crate gsparser;
+
+use glob::glob;
+use gsparser::{
+    bsp::BspReader,
+    mdl::null_terminated_bytes_to_str,
+};
+use std::{borrow::Cow, env, path::PathBuf};
+
+fn main() {
+    let args = env::args().skip(1).collect::<Vec<_>>();
+    let game_root = args.get(0).unwrap();
+    let output_path = args.get(1).unwrap();
+
+    let maps = collect_maps(&game_root);
+
+    // Ensure output path
+    let mut output_path = PathBuf::from(output_path);
+    if !output_path.exists() {
+        std::fs::create_dir(&output_path).expect("Failed to make output directory!");
+    }
+
+    output_path.push("dummy");
+    for bsp_path in &maps {
+        let map_name = bsp_path.file_stem().unwrap().to_str().unwrap();
+        let data = std::fs::read(bsp_path).unwrap();
+        let reader = BspReader::read(data);
+
+        let entity_string = resolve_map_entity_string(&reader);
+        output_path.set_file_name(format!("{}.txt", map_name));
+        std::fs::write(&output_path, entity_string.to_string()).unwrap();
+    }
+}
+
+fn collect_maps(path: &str) -> Vec<PathBuf> {
+    let mut paths = Vec::new();
+    let search = format!("{}/**/*.bsp", path);
+    let bsps = glob(&search).unwrap();
+    for bsp in bsps {
+        let bsp = bsp.unwrap();
+        paths.push(bsp);
+    }
+    paths
+}
+
+fn resolve_map_entity_string<'a>(reader: &'a BspReader) -> Cow<'a, str> {
+    let entities_bytes = reader.read_entities();
+    match null_terminated_bytes_to_str(entities_bytes) {
+        Ok(entities) => Cow::Borrowed(entities),
+        Err(error) => {
+            println!("  WARNING: {:?}", error);
+            let start = error.str_error.valid_up_to();
+            let end = start + error.str_error.error_len().unwrap_or(1);
+            println!("           error bytes: {:?}", &entities_bytes[start..end]);
+            String::from_utf8_lossy(&entities_bytes[..error.end])
+        }
+    }
+}
---
+++ examples/mdlheader.rs
@@ -0,0 +1,54 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::{fmt::Write, path::PathBuf};
+
+use gsparser::mdl::MdlHeader;
+
+fn main() {
+    let args: Vec<_> = std::env::args().skip(1).collect();
+    let game_root = args.get(0).unwrap();
+    let output_path = args.get(1).unwrap();
+
+    let models_path = {
+        let mut path = PathBuf::from(game_root);
+        path.push("models");
+        path
+    };
+
+    let mut model_paths = Vec::new();
+    for entry in std::fs::read_dir(models_path).unwrap() {
+        let entry = entry.unwrap();
+        if entry.file_type().unwrap().is_file() {
+            let entry_path = entry.path();
+            let extension = entry_path.extension().unwrap().to_str().unwrap();
+            if extension == "mdl" {
+                let file_stem = entry_path.file_stem().unwrap().to_str().unwrap();
+                model_paths.push((file_stem.to_owned(), entry_path));
+            }
+        }
+    }
+
+    // Ensure output path
+    let mut output_path = PathBuf::from(output_path);
+    if !output_path.exists() {
+        std::fs::create_dir(&output_path).expect("Failed to make output directory!");
+    }
+
+    // Print headers to output path
+    output_path.push("dummy");
+    for (stem, path) in model_paths {
+        println!("{}...", stem);
+        let mut text = String::new();
+
+        let file = File::open(path).unwrap();
+        let mut file = BufReader::new(file);
+        let header: MdlHeader = bincode::deserialize_from(&mut file).unwrap();
+        write!(&mut text, "{}", stem).unwrap();
+        write!(&mut text, "{:#?}", header).unwrap();
+
+        output_path.set_file_name(format!("{}.txt", stem));
+        std::fs::write(&output_path, text).unwrap();
+    }    
+
+    println!("Done!");
+}
